f5244c6d65cbd05edab4abacdebcad210d8be47e
JeffPhat <43369494+JeffPhat@users.noreply.github.com>
2018-09-24 15:46:56 +0700

    Create XXX.java

---
+++ XXX.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+
+
+class XXX{
+	public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    String name ="";
+    System.out.println("Input name: ");
+    name=sc.nextLine();
+		System.out.println("Hello World,I am !"+name);
+	}
+}
